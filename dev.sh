db3bf14a326b823d20dbc693c30593addef24829
Antoine2 Maurel <antoine.maurel@cacd2.fr>
2025-07-21 15:07:47 +0200

    chore: git aliases

--- dev.sh
+++ dev.sh
@@ -57,10 +57,25 @@ setup_ohmyzsh() {
   fi
 }
 
+git_aliases() {
+  git config — global push.default current
+  git config — global core.excludesfile ~/.gitignore
+  git config — global color.branch auto
+  git config — global color.diff auto
+  git config — global color.interactive auto
+  git config — global color.status auto
+  git config — global alias.st status
+  git config — global alias.ci commit
+  git config — global alias.co checkout
+  git config — global alias.br branch
+  git config — global alias.lg "log — graph — pretty=format:’%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue <%an>%Creset’ — abbrev-commit — date=relative"
+}
+
 install_homebrew
 brew update
 brew_install
 setup_ohmyzsh
+git_aliases
 
 # brew/ls
 {
